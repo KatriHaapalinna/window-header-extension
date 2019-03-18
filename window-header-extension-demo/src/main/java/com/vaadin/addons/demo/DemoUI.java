3e5f769f08a1917eba8e5a261f36e17ba653a2d3
Katri Haapalinna <katri@vaadin.com>
2019-03-18 14:16:36 +0200

    Refactor repo structure and comply to new directory release structure

---
+++ window-header-extension-demo/src/main/java/com/vaadin/addons/demo/DemoUI.java
@@ -0,0 +1,51 @@
+package com.vaadin.addons.demo;
+
+import javax.servlet.annotation.WebServlet;
+
+import org.vaadin.addons.windowheaderextension.WindowHeaderExtension;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+@Theme("demo")
+@Title("WindowHeaderExtension Demo")
+@SuppressWarnings("serial")
+public class DemoUI extends UI {
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = DemoUI.class)
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setStyleName("demoContentLayout");
+        layout.setSizeFull();
+        
+        Button button = new Button("Open window", e -> {
+        	Window w = new Window("Window Header", new Label("Window Content"));
+        	w.setHeight("25%");
+        	w.setWidth("25%");
+        	w.center();
+        	WindowHeaderExtension.extend(w, FontAwesome.GITHUB, () -> {
+        		Notification.show("You clicked a custom button!");
+        	});
+        	UI.getCurrent().addWindow(w);
+        });
+        layout.addComponents(button);
+        setContent(layout);
+    }
+
+}
